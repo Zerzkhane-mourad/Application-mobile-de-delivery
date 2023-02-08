37400850331710c9cc8fa7cd6a5d1616b1dbfc2c
zerzkhane <zezkhanemourad@gmail.com>
2023-02-08 16:00:37 +0100

    structure project

---
+++ src/routes/User.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { Request, Response  } from "express";
+import { defaults } from "joi";
+
+const routes = express.Router();    
+
+
+
+
+
+
+
+export default routes
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import { config } from './config/config'
+import Logging from './library/Logging'
+
+const router = express()
+
+mongoose.connect(config.mongo.url , { retryWrites: true, w: 'majority' } )
+    .then(()=>{
+        Logging.info("connected to base donne")
+    })
+    .catch((err)=> {
+        Logging.error(err)
+    })
